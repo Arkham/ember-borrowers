954ceff15f40dc73049656c59d4ec55a601e9e37
Ju Liu <ju.liu@welaika.com>
2015-07-16 17:42:43 +0100

    Adding, showing and editing friends is working

---
+++ app/controllers/friends/base.js
@@ -0,0 +1,36 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  isValid: Ember.computed(
+    'model.email',
+    'model.firstName',
+    'model.lastName',
+    'model.twitter',
+    function() {
+      return Ember.isPresent(this.get('model.email')) &&
+        Ember.isPresent(this.get('model.firstName')) &&
+        Ember.isPresent(this.get('model.lastName')) &&
+        Ember.isPresent(this.get('model.twitter'));
+    }
+  ),
+
+  actions: {
+    save: function() {
+      console.log('+- save called from friends base controller');
+
+      if (this.get('isValid')) {
+        this.get('model').save().then((friend) => {
+          this.transitionToRoute('friends.show', friend);
+        });
+      } else {
+        this.set('errorMessage', 'You have to fill all the fields');
+      }
+
+      return false;
+    },
+
+    cancel: function() {
+      return true;
+    }
+  }
+});
--- app/controllers/friends/edit.js
+++ app/controllers/friends/edit.js
@@ -3,9 +3,9 @@ import FriendsBaseController from './base';
 export default FriendsBaseController.extend({
   actions: {
     cancel: function() {
-      console.log('+- cancel called from friends new controller');
+      console.log('+- cancel called from friends show controller');
 
-      this.transitionToRoute('friends');
+      this.transitionToRoute('friends.show', this.get('model'));
 
       return false;
     }
