f7a6620863e034b389dd2585d0a18a05f20a8d9e
Ju Liu <ju.liu@welaika.com>
2015-07-20 16:21:38 +0100

    Use deactivate to discard changes

--- app/routes/friends/new.js
+++ app/routes/friends/new.js
@@ -5,6 +5,20 @@ export default Ember.Route.extend({
     return this.store.createRecord('friend');
   },
 
+  activate: function() {
+    console.log('+-- activate hook called from friends new route');
+  },
+
+  deactivate: function() {
+    console.log('+-- deactivate hook called from friends new route');
+
+    var model = this.modelFor('friends/new');
+
+    if (model.get('isNew')) {
+      model.destroyRecord();
+    }
+  },
+
   actions: {
     save: function() {
       console.log('+-- save called from friends new route');
