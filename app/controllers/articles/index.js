00fdf1e82e87bef97a1253356c21e18b2b796077
Ju Liu <ju.liu@welaika.com>
2015-07-20 21:07:56 +0100

    More complex observers

--- app/controllers/articles/index.js
+++ app/controllers/articles/index.js
@@ -2,5 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   queryParams: ['show'],
-  possibleStates: ['borrowed', 'returned']
+  possibleStates: ['borrowed', 'returned'],
+
+  contentDidChange: Ember.observer('model.[]', function() {
+    console.log('called when we add or remove an article');
+  }),
+
+  stateDidChange: Ember.observer('model.@each.state', function() {
+    console.log('called when the state property changes for any of the articles');
+  })
 });
