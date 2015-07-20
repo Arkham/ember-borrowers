fdeeb0a08c4fbd73730036e574a8f76fe60182cd
Ju Liu <ju.liu@welaika.com>
2015-07-20 12:24:08 +0100

    Save article status change

--- app/controllers/friends/base.js
+++ app/controllers/friends/base.js
@@ -1,10 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  hasEmail: Ember.computed.notEmpty('model.email');
-  hasFirstName: Ember.computed.notEmpty('model.firstName');
-  hasLastName: Ember.computed.notEmpty('model.LastName');
-  hasTwitter: Ember.computed.notEmpty('model.twitter');
+  hasEmail: Ember.computed.notEmpty('model.email'),
+  hasFirstName: Ember.computed.notEmpty('model.firstName'),
+  hasLastName: Ember.computed.notEmpty('model.LastName'),
+  hasTwitter: Ember.computed.notEmpty('model.twitter'),
 
   isValid: Ember.computed.and(
     'hasEmail',
