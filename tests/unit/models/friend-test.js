0efaa02366401a557d9a26826dfc06940465e70e
Ju Liu <ju.liu@welaika.com>
2015-07-21 12:05:12 +0100

    Add a couple of model tests

--- tests/unit/models/friend-test.js
+++ tests/unit/models/friend-test.js
@@ -1,3 +1,4 @@
+import Ember from 'ember';
 import { moduleForModel, test } from 'ember-qunit';
 
 moduleForModel('friend', 'Unit | Model | friend', {
@@ -7,4 +8,28 @@ moduleForModel('friend', 'Unit | Model | friend', {
 test('it exists', function(assert) {
   var model = this.subject();
   assert.ok(!!model);
+});
+
+test('fullName joins firstName and lastName', function(assert) {
+  var model = this.subject({
+    firstName: 'Syd',
+    lastName: 'Barrett'
+  });
+
+  assert.equal(model.get('fullName'), 'Syd Barrett');
+
+  Ember.run(function() {
+    model.set('firstName', 'John');
+  });
+
+  assert.equal(model.get('fullName'), 'John Barrett');
+});
+
+test('has many articles', function(assert) {
+  var klass = this.subject().constructor;
+
+  var relationship = Ember.get(klass, 'relationshipsByName').get('articles');
+
+  assert.equal(relationship.key, 'articles');
+  assert.equal(relationship.kind, 'hasMany');
 });
